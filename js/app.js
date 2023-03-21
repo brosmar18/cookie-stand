8323f4b5814da8c2aeb2fa541743a69d0e53f609
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-03-21 14:29:55 -0500

    storeParis and storeLima completed

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ let storeSeattle = {
     let article = document.createElement('article');
     parentElement.appendChild(article);
     let storeDescription = document.createElement('p');
-    storeDescription.textContent = 'Salmon Cookies Co. in Seattle is a unique bakery that specializes in delicious and nutritious salmon-based cookies.';
+    storeDescription.textContent = 'Seattle';
     article.appendChild(storeDescription);
     let storeSalesList = document.createElement('ul');
     article.appendChild(storeSalesList);
@@ -60,7 +60,7 @@ let storeTokyo = {
     let article = document.createElement('article');
     parentElement.appendChild(article);
     let storeDescription = document.createElement('p');
-    storeDescription.textContent = 'Salmon Cookies Co. in Tokyo is a unique bakery that specializes in delicious and nutritious salmon-based cookies.';
+    storeDescription.textContent = 'Tokyo';
     article.appendChild(storeDescription);
     let storeSalesList = document.createElement('ul');
     article.appendChild(storeSalesList);
@@ -98,7 +98,7 @@ let storeDubai = {
     let article = document.createElement('article');
     parentElement.appendChild(article);
     let storeDescription = document.createElement('p');
-    storeDescription.textContent = 'Salmon Cookies Co. in Dubai is a unique bakery that specializes in delicious and nutritious salmon-based cookies.';
+    storeDescription.textContent = 'Dubai';
     article.appendChild(storeDescription);
     let storeSalesList = document.createElement('ul');
     article.appendChild(storeSalesList);
@@ -118,20 +118,82 @@ let storeDubai = {
 };
 
 storeDubai.storeData();
+
 let storeParis = {
-  minCust: 20,
+  minCust: 11,
   maxCust: 38,
   avgCookieSale: 3.7,
-  cookieTotal: []
+  totalDailyCookies: 0,
+  cookieTotal: [],
+  cookiesPurchase: function() {
+    for(let i = 0; i < hours.length; i++){
+      console.log(i);
+      this.cookieTotal[i] = Math.floor(randomCustNum(this.minCust, this.maxCust) * this.avgCookieSale);
+    }
+  },
+  storeData: function(){
+    this.cookiesPurchase();
+    let article = document.createElement('article');
+    parentElement.appendChild(article);
+    let storeDescription = document.createElement('p');
+    storeDescription.textContent = 'Paris';
+    article.appendChild(storeDescription);
+    let storeSalesList = document.createElement('ul');
+    article.appendChild(storeSalesList);
+    for (let i = 0; i < hours.length; i++) {
+      console.log(hours[i]);
+      let storeListItem = document.createElement('li');
+      this.totalDailyCookies += storeParis.cookieTotal[i];
+      storeListItem.textContent = `${hours[i]} : ${storeParis.cookieTotal[i]}`;
+      storeSalesList.appendChild(storeListItem);
+    }
+    let storetotalDailyCookies = document.createElement('li');
+    storetotalDailyCookies.textContent = `Total: ${this.totalDailyCookies}`;
+    storeSalesList.appendChild(storetotalDailyCookies);
+
+  }
 
 };
+
+storeParis.storeData();
+
 let storeLima = {
-  minCust: 2,
-  maxCust: 16,
-  avgCookieSale: 4.6,
-  cookieTotal: []
+  minCust: 11,
+  maxCust: 38,
+  avgCookieSale: 3.7,
+  totalDailyCookies: 0,
+  cookieTotal: [],
+  cookiesPurchase: function() {
+    for(let i = 0; i < hours.length; i++){
+      console.log(i);
+      this.cookieTotal[i] = Math.floor(randomCustNum(this.minCust, this.maxCust) * this.avgCookieSale);
+    }
+  },
+  storeData: function(){
+    this.cookiesPurchase();
+    let article = document.createElement('article');
+    parentElement.appendChild(article);
+    let storeDescription = document.createElement('p');
+    storeDescription.textContent = 'Lima';
+    article.appendChild(storeDescription);
+    let storeSalesList = document.createElement('ul');
+    article.appendChild(storeSalesList);
+    for (let i = 0; i < hours.length; i++) {
+      console.log(hours[i]);
+      let storeListItem = document.createElement('li');
+      this.totalDailyCookies += storeLima.cookieTotal[i];
+      storeListItem.textContent = `${hours[i]} : ${storeLima.cookieTotal[i]}`;
+      storeSalesList.appendChild(storeListItem);
+    }
+    let storetotalDailyCookies = document.createElement('li');
+    storetotalDailyCookies.textContent = `Total: ${this.totalDailyCookies}`;
+    storeSalesList.appendChild(storetotalDailyCookies);
+
+  }
 
 };
+
+storeLima.storeData();
 
 function randomCustNum(minCust, maxCust) {
   return Math.floor(Math.random() * (maxCust - minCust) + minCust);
